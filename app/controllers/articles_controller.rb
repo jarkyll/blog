1ca83b3511a94b6aeb1fe7720854471a57f43d14
Nabeel Virani <nabeelvirani@gmail.com>
2015-10-17 15:19:48 -0500

    part 2 of having a working submission

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,19 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect_to @article
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
+
+    def show
+      #finds the article that we are most interested in
+      @article = Article.find(params[:id])
+    end
 end
